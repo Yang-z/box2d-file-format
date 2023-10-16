56d959781647de271b68bd05c40b125336995021
Yang-z <zheng_yang_yang@msn.com>
2023-10-17 00:44:15 +0800

    update test files

--- test/test.cpp
+++ test/test.cpp
@@ -4,26 +4,11 @@
 
 #include "db2_hardware_difference.h"
 #include "db2_data_structure.h"
-#include "db2_parser.h"
+#include "db2_decoder.h"
 
-template <typename T>
-auto print(char const *const message, const T &t) -> void
-{
-    std::cout << message;
-    if (std::is_same<T, bool>::value)
-    {
-        std::cout << (t ? "true" : "false");
-    }
-    else if (std::is_same<T, uint8_t>::value)
-    {
-        std::cout << +t;
-    }
-    else
-    {
-        std::cout << t;
-    }
-    std::cout << std::endl;
-}
+#include <boost/pfr.hpp> // reflect
+// #include <boost/pfr/core.hpp>
+// #include <boost/pfr/core_name.hpp>
 
 auto test_hardware_difference() -> void
 {
@@ -33,23 +18,49 @@ auto test_hardware_difference() -> void
     // std::cout << "Date Structure Alignment offset: " << +hardwareDifference::getDataStructureAlignment() << std::endl;
     // std::cout << "Date Structure Alignment offset (packed): " << +hardwareDifference::getDataStructureAlignment(true) << std::endl;
 
-    print("Is little endian: ", hardwareDifference::isLittleEndian());
-    print("Is little endian (bit endianness): ", hardwareDifference::isLittleEndian_Bit());
-    print("Date Structure Alignment offset: ", hardwareDifference::getDataStructureAlignment());
-    print("Date Structure Alignment offset (packed): ", hardwareDifference::getDataStructureAlignment(true));
+    printf("Is little endian: %s\n", hardwareDifference::isLittleEndian() ? "true" : "false");
+    printf("Is little endian (bit endianness): %s\n", hardwareDifference::isLittleEndian_Bit() ? "true" : "false");
+    printf("Date Structure Alignment offset: %d\n", hardwareDifference::getDataStructureAlignment());
+    printf("Date Structure Alignment offset (packed): %d\n", hardwareDifference::getDataStructureAlignment(true));
+}
+
+auto test_reflection() -> void
+{
+    struct S0
+    {
+        int i0{8};
+        bool b0{true};
+
+        struct
+        {
+            int i1{4};
+            float b2{2.0f};
+        } s1;
+    } s0;
+
+    int count = 0;
+    boost::pfr::for_each_field(
+        s0,
+        [&s0, &count](auto &chunk)
+        {
+            ++count;
+            printf("%d\n", sizeof(chunk));
+            // printf("%d\n", boost::pfr::get_name<count, s0>());
+        });
+    printf("count: %d\n", count);
 }
 
 auto test_data_structure_write() -> void
 {
     auto db2 = new dotBox2d();
-    db2->chunks.info.push();
-    db2->chunks.world.push();
+    db2->chunks.info.emplace_back();
+    db2->chunks.world.emplace_back();
     for (int i = 0; i < 2; i++)
-        db2->chunks.body.push();
+        db2->chunks.body.emplace_back();
     for (int i = 0; i < 3; i++)
-        db2->chunks.fixture.push();
+        db2->chunks.fixture.emplace_back();
     for (int i = 0; i < 16; i++)
-        db2->chunks.vector.push();
+        db2->chunks.vector.emplace_back();
     db2->save("./test.B2d");
     delete db2;
     db2 = nullptr;
@@ -61,13 +72,89 @@ auto test_data_structure_read() -> void
     return;
 }
 
+auto test_step(b2World *b2w) -> void
+{
+    auto dynamicBody = b2w->GetBodyList()->GetNext();
+
+    int t = 0;
+    while (true)
+    {
+        b2w->Step(1.0f / 60.0f, 6, 2);
+        printf("Py = %f; Vy = %f\n", dynamicBody->GetPosition().y, dynamicBody->GetLinearVelocity().y);
+        if (t > 200)
+            break;
+        t++;
+    }
+    printf("step count: %d\n", t);
+}
+
+auto test_encoding() -> void
+{
+    dotB2Decoder der{};
+    der.b2w = new b2World{{0.0f, -9.8f}};
+
+    /*body*/
+    b2BodyDef dynamicBodyDef{};
+    dynamicBodyDef.type = b2_dynamicBody;
+    dynamicBodyDef.position.Set(8.0f, 8.0f);
+
+    auto dynamicBody = der.b2w->CreateBody(&dynamicBodyDef);
+
+    /*fixture*/
+    b2CircleShape circle{};
+    circle.m_radius = 0.5f;
+
+    b2FixtureDef fixturedef{};
+    fixturedef.shape = &circle;
+    fixturedef.density = 1.0f;
+    fixturedef.restitution = 0.8f;
+
+    dynamicBody->CreateFixture(&fixturedef);
+
+    /*body2*/
+    b2BodyDef staticBodyDef{};
+    staticBodyDef.type = b2_staticBody;
+    staticBodyDef.position.Set(0.0f, 0.0f);
+
+    auto staticBody = der.b2w->CreateBody(&staticBodyDef);
+
+    /*fixture2*/
+    b2ChainShape chain{};
+    // b2Vec2 vs[4]{{0.0f, 0.0f}, {16.0f, 0.0f}, {16.0f, 9.0f}, {0.0f, 9.0f}}; // anti-clockw, ouside
+    b2Vec2 vs[4]{{0.0f, 0.0f}, {0.0f, 9.0f}, {16.0f, 9.0f}, {16.0f, 0.0f}}; // inside
+    chain.CreateLoop(vs, 4);
+
+    b2FixtureDef fixturedef2{};
+    fixturedef2.shape = &chain;
+
+    staticBody->CreateFixture(&fixturedef2);
+
+    der.encode();
+    der.db2->save("./test_encode.B2d");
+
+    test_step(der.b2w);
+}
+
+auto test_decoding() -> void
+{
+    dotB2Decoder der{};
+    der.db2 = new dotBox2d{"./test_encode.B2d"};
+    der.decode();
+
+    test_step(der.b2w);
+}
+
 auto main() -> int
 {
     test_hardware_difference();
-    // test_data_structure_write();
-    test_data_structure_read();
 
-    dotB2Parser p{};
+    // test_reflection();
+
+    // test_data_structure_write();
+    // test_data_structure_read();
+
+    test_encoding();
+    test_decoding();
 
     return 0;
 }
